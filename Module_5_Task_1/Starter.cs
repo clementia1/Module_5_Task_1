fdfe9f57013c75cc609bde66d0eb1f2c3b22b7c1
clementia1 <grininma0@gmail.com>
2021-05-12 09:51:19 +0300

    Update Starter.cs

--- Module_5_Task_1/Starter.cs
+++ Module_5_Task_1/Starter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 using Module_5_Task_1.Services.Abstractions;
 using Module_5_Task_1.Services.Implementations;
 using Module_5_Task_1.Dto.User;
@@ -28,27 +29,30 @@ namespace Module_5_Task_1
 
         public async Task Run()
         {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
             var requests = new List<Task>()
             {
-                Task.Run(() => _userService.GetByPage(2)),
-                Task.Run(() => _userService.GetById(1)),
-                Task.Run(() => _userService.GetById(999)),
-                Task.Run(() => _unknownService.GetByPage(1)),
-                Task.Run(() => _unknownService.GetById(6)),
-                Task.Run(() => _unknownService.GetById(666)),
-                Task.Run(() => _userService.Add(new CreateUserRequest { Name = "morpheus", Job = "leader" })),
-                Task.Run(() => _userService.Update(new UpdateUserRequest { Name = "morpheus", Job = "zion resident" }, 3)),
-                Task.Run(() => _userService.Patch(new UpdateUserRequest { Name = "morpheus", Job = "zion resident" }, 3)),
-                Task.Run(() => _userService.Delete(1)),
-                Task.Run(() => _registrationService.Register(new RegistrationRequest { Email = "eve.holt@reqres.in", Password = "123456" })),
-                Task.Run(() => _registrationService.Register(new RegistrationRequest { Email = "eve.holt@reqres.in" })),
-                Task.Run(() => _authorizationService.Authorize(new AuthorizationRequest { Email = "eve.holt@reqres.in", Password = "cityslicka" })),
-                Task.Run(() => _authorizationService.Authorize(new AuthorizationRequest { Password = "cityslicka" })),
-                Task.Run(() => _userService.GetByPageWithDelay(1, 3)),
+                Task.Run(async () => await _userService.GetByPage(2)),
+                Task.Run(async () => await _userService.GetById(1)),
+                Task.Run(async () => await _userService.GetById(999)),
+                Task.Run(async () => await _unknownService.GetByPage(1)),
+                Task.Run(async () => await _unknownService.GetById(6)),
+                Task.Run(async () => await _unknownService.GetById(666)),
+                Task.Run(async () => await _userService.Add(new CreateUserRequest { Name = "morpheus", Job = "leader" })),
+                Task.Run(async () => await _userService.Update(new UpdateUserRequest { Name = "morpheus", Job = "zion resident" }, 3)),
+                Task.Run(async () => await _userService.Patch(new UpdateUserRequest { Name = "morpheus", Job = "zion resident" }, 3)),
+                Task.Run(async () => await _userService.Delete(1)),
+                Task.Run(async () => await _registrationService.Register(new RegistrationRequest { Email = "eve.holt@reqres.in", Password = "123456" })),
+                Task.Run(async () => await _registrationService.Register(new RegistrationRequest { Email = "eve.holt@reqres.in" })),
+                Task.Run(async () => await _authorizationService.Authorize(new AuthorizationRequest { Email = "eve.holt@reqres.in", Password = "cityslicka" })),
+                Task.Run(async () => await _authorizationService.Authorize(new AuthorizationRequest { Password = "cityslicka" })),
+                Task.Run(async () => await _userService.GetByPageWithDelay(1, 7)),
             };
 
             await Task.WhenAll(requests);
-            Console.WriteLine("All tasks completed");
+            Console.WriteLine("All tasks completed in: " + stopwatch.Elapsed);
         }
     }
 }
